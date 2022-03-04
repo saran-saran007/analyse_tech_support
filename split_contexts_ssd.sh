62cf982a0171a5aabc1e2e3ecc92ddd00e2c72e6
Saravanan Murugesan <sarmurug@cisco.com>
2022-03-04 14:33:06 +0530

    updated split ctx

--- split_contexts_ssd.sh
+++ split_contexts_ssd.sh
@@ -1,9 +1,12 @@
 #/bin/bash
 
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
-printf "$0 <path-to-support_summary-file\n"
-exit
+  printf "$0 <path-to-support_summary-file> <context-prefix>\n"
+  exit
 fi
 
-csplit --digits=2  --quiet --prefix=ctx_ $1 "/\* Context: /+1" "{*}"
+printf -v exec_cmd "csplit --digits=2  --quiet --prefix=ctx_$2 $1 \"/\* Context: $2/+1\" \"{*}\""
+
+echo "Executing command $exec_cmd"
+eval $exec_cmd
